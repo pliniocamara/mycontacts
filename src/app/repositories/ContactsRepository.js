c914ff1ba08de2acd15097271aea3f76d9cabf1b
pliniocamara <plinio.camara@outlook.com>
2023-01-16 11:36:05 -0300

    finalizacao api utilizando mock

---
+++ src/app/repositories/ContactsRepository.js
@@ -0,0 +1,84 @@
+const { v4 } = require('uuid');
+
+let contacts = [
+  {
+    id: v4(),
+    name: 'Plínio',
+    email: 'plinio@mail.com',
+    phone: '5585900000000',
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'José',
+    email: 'jose@mail.com',
+    phone: '5585900000000',
+    category_id: v4(),
+  },
+];
+
+class ContactsRepository {
+  findAll() {
+    return new Promise((resolve) => {
+      resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  findByEmail(email) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.email === email));
+    });
+  }
+
+  create({
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const newContact = {
+        id: v4(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts.push(newContact);
+      resolve(newContact);
+    });
+  }
+
+  update(id, {
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const updatedContact = {
+        id,
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts = contacts.map((contact) => (
+        contact.id === id ? updatedContact : contact
+      ));
+
+      resolve(updatedContact);
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
+  }
+}
+
+module.exports = new ContactsRepository();
