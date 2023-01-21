d42b205d4fdc9799311ecd46d38f5c099b5a3fe3
pliniocamara <plinio.camara@outlook.com>
2023-01-21 13:17:41 -0300

    finalizando api com categorias e tratamento de erros

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -6,5 +7,9 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
